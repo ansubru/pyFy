Turbulent part works (but gives the wrong result) --> GS is now faster --> Maybe something wrong with k and omega model

src/FVD/calcResiduals.py
@@ -34,21 +34,20 @@ class calcResiduals(object):
     def __init__(self):
         """Return object"""
 
-    def calcRes(self, var ,aW, aE, aN, aS, SU, aP):
+    def calcRes(self, var ,aW, aE, aN, aS, SU, aP, B, flag):
         """Function to calculate residuals given a variable"""
 
-        i = np.size(var, 0)
-        j = np.size(var, 1)
-
-        Rvar = 0
-
-        for m in range(1,i-1):  # loop through rows
-            for n in range(1,j-1):  # loop through columns
-
-                    sumvar = aE[m][n]*var[m][n+1] + aW[m][n]*var[m][n-1] + aN[m][n]*var[m-1][n] + aS[m][n]*var[m+1][n] + SU[m][n] -aP[m][n]*var[m][n]
-                    Rvar = Rvar + abs(sumvar)
-
-        return (Rvar)
+        sumvar = 0.0 * var
+        if flag in ['omega']:
+            sumvar[2:-2, 2:-2] = abs((var[2:-2, 1:-3] * aW[2:-2, 2:-2] + var[2:-2, 3:-1] * aE[2:-2, 2:-2] + var[3:-1, 2:-2] * aS[2:-2, 2:-2] + \
+                                  var[1:-3, 2:-2] * aN[2:-2, 2:-2] + SU[2:-2, 2:-2]) - aP[2:-2, 2:-2]*var[2:-2, 2:-2])
+        elif flag in ['B', 'b']:
+            sumvar = B
+        else:
+            sumvar[1:-1, 1:-1] = abs((var[1:-1, 0:-2] * aW[1:-1, 1:-1] + var[1:-1, 2:] * aE[1:-1, 1:-1] + var[2:, 1:-1] * aS[1:-1,1:-1] + \
+                                              var[0:-2, 1:-1] * aN[1:-1, 1:-1] + SU[1:-1, 1:-1]) - aP[1:-1, 1:-1]*var[1:-1, 1:-1])
+        Rvar = sumvar.sum()
+        return Rvar
 
     def calcResomg(self, var ,aW, aE, aN, aS, SU, aP):
         """Function to calculate residuals given a variable"""
@@ -76,8 +75,6 @@ class calcResiduals(object):
 
         for m in range(1,i-1):  # loop through rows
             for n in range(1,j-1):  # loop through columns
-
-                if (m != 0 and n != 0 and m != (i - 1) and n != (j - 1)):  # Internal nodes
                     sumvar = B[m][n]
                     Rvar = Rvar + abs(sumvar)
         return (Rvar)

src/FVD/gaussSeidel2.py
@@ -19,89 +19,112 @@ import sys
 import pprint
 import scipy.sparse as sps
 import scipy.sparse.linalg as spla
+from multiprocessing import Pool
+
 
 #############################################################################################################################################################################################################
 ###----------------------------------------------------------------------------CLASS DEFINITION-----------------------------------------------------------------------------------------------###
+def computeError(u, uold):
+    """Computes absolute error using an L2 norm for the solution.
+    This requires that self.u and self.old_u must be appropriately
+    setup."""
+    v = abs(u - uold)/max(uold.any(), 1e-12)
+    sumRes = v.sum()
+    return sumRes
 
 class gaussSiedel2(object):
 
     def __init__(self):
         """Return object"""
 
-    def gaussSeidel3u(self, u, aW, aE, aN, aS, aP, SUx, iters):
+
+    def gaussSeidel3u(self, u, aW, aE, aN, aS, aP, SUx, iters, flag):
         """Solves gauss seidel for a fixed number of iterations."""
 
-        ###############------------CREATE IO OBJECT--------------################
-        from IO import IO
-        IO_obj = IO("random")
-
-        #Initialize all relevant variables:u, v, p etc.
-        i = np.size(u, 0)
-        j = np.size(u, 1)
-        ugs = 0.0*u
-        uold = 0.0*u
-
         iter = 0 # Mock up error parameter only to run the While
-        residual = 1
-        sumRes = 0.0
 
         while iters > iter:
 
-            for m in range(i):  # loop through rows
-                for n in range(j):  # loop through columns
-                    if (m != 0 and n != 0 and m != (i - 1) and n != (j - 1)):  #Internal nodes:
+            if flag in ['omega']:
 
-                        uN = u[m - 1][n]
-                        uS = u[m + 1][n]
-                        uE = u[m][n + 1]
-                        uW = u[m][n - 1]
-
-                        u[m][n]=(uW* aW[m][n]+ uE*aE[m][n]+uS*aS[m][n]+\
-                                           uN*aN[m][n]+SUx[m][n])/aP[m][n]
-                        sumRes = sumRes + (uW* aW[m][n]+ uE*aE[m][n]+uS*aS[m][n]+\
-                                           uN*aN[m][n]+SUx[m][n]) - u[m][n]*aP[m][n]
+                u[2:-2, 2:-2] = (u[2:-2, 1:-3] * aW[2:-2, 2:-2] + u[2:-2, 3:-1] * aE[2:-2, 2:-2] + u[3:-1, 2:-2] * aS[2:-2, 2:-2] + \
+                             u[1:-3, 2:-2] * aN[2:-2, 2:-2] + SUx[2:-2, 2:-2]) / aP[2:-2, 2:-2]
+            else:
+                u[1:-1, 1:-1] = (u[1:-1, 0:-2] * aW[1:-1, 1:-1] + u[1:-1, 2:] * aE[1:-1, 1:-1] + u[2:, 1:-1] * aS[1:-1,1:-1] + \
+                                 u[0:-2, 1:-1] * aN[1:-1, 1:-1] + SUx[1:-1, 1:-1]) / aP[1:-1, 1:-1]
             iter += 1
-            residual = abs(sumRes)
 
-        print "Solved G-S with %i iterations"%(iter)
+        print "Solved G-S for %s with %i iterations"%(flag,iter)
         return u
 
     def gaussSeidel4u(self, u, aW, aE, aN, aS, aP, SUx, resinp, flag):
         """Solves gauss seidel for a fixed number of iterations."""
 
+        iter = 0 # Mock up error parameter only to run the While
+        diffs = 1
+        residual = 1
+        resHist  = [diffs]
+
+        while residual > resinp:
+            uold = u
+            if flag in ['omega']:
+
+                u[2:-2, 2:-2] = (u[2:-2, 1:-3] * aW[2:-2, 2:-2] + u[2:-2, 3:-1] * aE[2:-2, 2:-2] + u[3:-1, 2:-2] * aS[2:-2, 2:-2] + \
+                             u[1:-3, 2:-2] * aN[2:-2, 2:-2] + SUx[2:-2, 2:-2]) / aP[2:-2, 2:-2]
+            else:
+                u[1:-1, 1:-1] = (u[1:-1, 0:-2] * aW[1:-1, 1:-1] + u[1:-1, 2:] * aE[1:-1, 1:-1] + u[2:, 1:-1] * aS[1:-1,1:-1] + \
+                                 u[0:-2, 1:-1] * aN[1:-1, 1:-1] + SUx[1:-1, 1:-1]) / aP[1:-1, 1:-1]
+
+            sumRes = computeError(u, uold)
+            residual = sumRes #/ np.size(u)  # update residual
+            print residual
+            # resHist.append((residual))
+            # diffs = abs(resHist[-1] - resHist[-2])
+            if iter > 5000 :
+                break
+
+            iter += 1
+        print "Solved G-S for %s with %i iterations and a final residual of %e"%(flag, iter, diffs)
+
+        return u
+
+    def gaussSeidel5u(self, u, aW, aE, aN, aS, aP, SUx, resinp, flag):
+        """Solves gauss seidel for a fixed number of iterations."""
+
         ###############------------CREATE IO OBJECT--------------################
         from IO import IO
         IO_obj = IO("random")
 
-        #Initialize all relevant variables:u, v, p etc.
+        # Initialize all relevant variables:u, v, p etc.
         i = np.size(u, 0)
         j = np.size(u, 1)
-        ugs = 1.0*u
-        uold = 0.0*u
+        ugs = 1.0 * u
+        uold = 0.0 * u
 
-        iter = 0 # Mock up error parameter only to run the While
+        iter = 0  # Mock up error parameter only to run the While
         diffs = 1
         sumRes = 1
-        resHist  = [diffs]
+        resHist = [diffs]
 
         while diffs > resinp:
-        #while 50 > iter:
+            # while 50 > iter:
             if flag in ['u', 'U', 'v', 'V', 'p', 'P', 'k', 'K']:
 
                 for m in range(i):  # loop through rows
                     for n in range(j):  # loop through columns
 
-                            if (m != 0 and n != 0 and m != (i - 1) and n != (j - 1)):  #Internal nodes (for u, v and k):
+                        if (m != 0 and n != 0 and m != (i - 1) and n != (
+                            j - 1)):  # Internal nodes (for u, v and k):
 
-                                uold[m][n] = ugs[m][n]
-                                uN = u[m - 1][n]
-                                uS = u[m + 1][n]
-                                uE = u[m][n + 1]
-                                uW = u[m][n - 1]
+                            uold[m][n] = ugs[m][n]
+                            uN = u[m - 1][n]
+                            uS = u[m + 1][n]
+                            uE = u[m][n + 1]
+                            uW = u[m][n - 1]
 
-                                ugs[m][n]=(uW* aW[m][n]+ uE*aE[m][n]+uS*aS[m][n]+\
-                                                   uN*aN[m][n]+SUx[m][n])/aP[m][n]
-                                sumRes = sumRes + (abs((uold[m][n] - ugs[m][n])) / max(1e-16, abs(uold[m][n])))
+                            ugs[m][n] = (uW * aW[m][n] + uE * aE[m][n] + uS * aS[m][n] + \
+                                         uN * aN[m][n] + SUx[m][n]) / aP[m][n]
+                            sumRes = sumRes + (abs((uold[m][n] - ugs[m][n])) / max(1e-16, abs(uold[m][n])))
 
             else:
                 for m in range(2, i - 2):  # loop through rows
@@ -117,21 +140,67 @@ class gaussSiedel2(object):
                                      uN * aN[m][n] + SUx[m][n]) / aP[m][n]
                         sumRes = sumRes + (abs((uold[m][n] - ugs[m][n])) / max(1e-16, abs(uold[m][n])))
             iter += 1
-            u = ugs #update u
-            residual = sumRes/np.size(u) #update residual
+            u = ugs  # update u
+            residual = sumRes / np.size(u)  # update residual
             resHist.append((residual))
             diffs = abs(resHist[-1] - resHist[-2])
+
+            if iter > 5000:
+                break
+
+        if flag in ['U', 'u']:
+            print "Solved G-S for U with %i iterations and a final residual of %e" % (iter, diffs)
+        elif flag in ['v', 'V']:
+            print "Solved G-S for V with %i iterations and a final residual of %e" % (iter, diffs)
+        elif flag in ['k', 'K']:
+            print "Solved G-S for K with %i iterations and a final residual of %e" % (iter, diffs)
+        else:
+            print "Solved G-S for omega with %i iterations and a final residual of %e" % (iter, diffs)
+
+        return u
+
+    def gaussSeidel6u(self, u, aW, aE, aN, aS, aP, SUx, resinp, flag):
+        """Solves gauss seidel for a fixed number of iterations."""
+
+        iter = 0 # Mock up error parameter only to run the While
+        # Initialize all relevant variables:u, v, p etc.
+        i = np.size(u, 0)
+        j = np.size(u, 1)
+        diffs = 1
+        residual = 1
+        resHist  = [diffs]
+
+        while residual > resinp:
+            if flag in ['omega']:
+                for m in range(2, i - 2):  # loop through rows
+                    for n in range(2, j - 2):  # loop through columns
+
+                        uN = u[m - 1][n]
+                        uS = u[m + 1][n]
+                        uE = u[m][n + 1]
+                        uW = u[m][n - 1]
+
+                        u[m][n] = (uW * aW[m][n] + uE * aE[m][n] + uS * aS[m][n] + \
+                                     uN * aN[m][n] + SUx[m][n]) / aP[m][n]
+
+            else:
+                for m in range(i):  # loop through rows
+                    for n in range(j):  # loop through columns
+                        if (m != 0 and n != 0 and m != (i - 1) and n != (j - 1)):  # Internal nodes:
+
+                            uN = u[m - 1][n]
+                            uS = u[m + 1][n]
+                            uE = u[m][n + 1]
+                            uW = u[m][n - 1]
+
+                            u[m][n] = (uW * aW[m][n] + uE * aE[m][n] + uS * aS[m][n] + \
+                                       uN * aN[m][n] + SUx[m][n]) / aP[m][n]
 
             if iter > 5000 :
                 break
 
-        if flag in ['U', 'u']:
-            print "Solved G-S for U with %i iterations and a final residual of %e"%(iter, diffs)
-        elif flag in ['v', 'V']:
-            print "Solved G-S for V with %i iterations and a final residual of %e"%(iter, diffs)
-        elif flag in ['k', 'K']:
-            print "Solved G-S for K with %i iterations and a final residual of %e"%(iter, diffs)
-        else:
-            print "Solved G-S for omega with %i iterations and a final residual of %e"%(iter, diffs)
+            iter += 1
+        print "Solved G-S for %s with %i iterations and a final residual of %e"%(flag, iter, diffs)
+
         return u
 

src/FVD/plotResults.py
@@ -19,6 +19,7 @@ import IO
 import Interpolate
 import pprint
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import draw, figure, show
 import pylab
 
 
@@ -129,7 +130,7 @@ class plotResults(object):
                 grid_y[n][m] = 1 - y
         return grid_x, grid_y
 
-    def plotdataTurb(self, U, V, k, omega, Y, mut, resU, resV, resK, resomega, resB, it):
+    def plotdataTurb(self, U, V, k, omega, Y, mut, resU, resV, resK, resomega, resB, resPP, it):
 
         i = np.size(U, 0)
         j = np.size(V, 1)
@@ -209,6 +210,7 @@ class plotResults(object):
         pylab.subplot(236)
         pylab.semilogy(iter[1:-1], resU[1:-1], 'blue', label='U-velocity')
         pylab.semilogy(iter[1:-1], resV[1:-1], 'black', label='V-velocity')
+        pylab.semilogy(iter[1:-1], resPP[1:-1], 'magenta', label='Pprime')
         pylab.semilogy(iter[1:-1], resK[1:-1], 'red', label='k')
         pylab.semilogy(iter[1:-1], resomega[1:-1], 'green', label='omega')
         pylab.semilogy(iter[1:-1], resB[1:-1], 'cyan', label='continuity')
@@ -216,6 +218,14 @@ class plotResults(object):
         pylab.ylabel('Residual')
         pylab.grid(True)
         pylab.grid(which='both')
+        pylab.show()
+
+        pylab.figure
+        pylab.plot(LarsData[:, 4], LarsData[:, 2], 'ro', label='lada omega')
+        pylab.plot(omegaplot[0:-1], y[0:-1], 'k.', label='my omega')
+        pylab.ylabel('y [m]')
+        pylab.xlabel('Turbulent dissipation rate [1/s]')
+        pylab.xlim(0, 10)
         pylab.legend(loc='upper right')
         pylab.show()
 
@@ -231,7 +241,7 @@ class plotResults(object):
         y = []
         Uplot = []
         Vplot = []
-        iter = list(range(1, it + 2))
+        iter = list(range(1, it + 1))
         from IO import IO
         IO_obj = IO("random")
         fxe = IO_obj.fxe